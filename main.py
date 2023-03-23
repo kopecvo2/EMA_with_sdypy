3dacd7adb376ecf8c521594e0ddaa3514099e999
kopecvo2 <81413409+kopecvo2@users.noreply.github.com>
2023-03-23 18:35:26 +0100

    Scrollbar to choose FRF in comparsion graph is working

--- main.py
+++ main.py
@@ -7,56 +7,88 @@ import pyuff
 import EMA
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import tkinter as tk
 
-# uff_file = pyuff.UFF('C:/Users/pc/Desktop/Scan_odlitek1_testP1_s1.UFF')
-#
-# uff_file.get_set_types()
-
-# data = uff_file.read_sets()
-
+# EMA
 acc = EMA.Model(lower=10,
                 upper=10000,
-                pol_order_high=50,
+                pol_order_high=100,
                 frf_from_uff=True)
 
 acc.read_uff('C:/Users/pc/Desktop/Scan_odlitek1_testP1_s1.UFF')
 
 acc.get_poles(method='lscf', show_progress=True)
 
-# acc.select_poles()
-
-approx_nat_freq = [1310, 1826, 3220, 3940, 5540, 6200, 6240, 7150, 7450, 7800, 8450, 8750, 8850, 9000, 9300, 9700]
+approx_nat_freq = [1310, 1826, 3220, 3940, 5540, 5860, 6200, 6240, 7150, 7450, 7800, 8450, 8750, 8850, 9000, 9300, 9700]
 acc.select_closest_poles(approx_nat_freq)
 
-H, A = acc.get_constants(method='lsfd', f_lower=1000)
-
-# FRF = acc.FRF_reconstruct(0)
+H, A = acc.get_constants(method='lsfd', f_lower=None)
 
 MAC = acc.autoMAC()
 
 acc.print_modal_data()
 
 
-def reconstruct(model, location):
-    reconstructed = np.abs(model.H[location, :])
-    frequencies = model.freq
-    measured = np.abs(model.frf[location, :])
+def reconstruct(model):
+    """
+    Function creates interactive canvas with comparation of measured and reconstructed FRFs from model for each FRF.
+    :param model: Object of class EMA.EMA.Model
+    :return: None
+    """
+    def plot(dummy, p):
+        """
+        plots desired FRF on canvas.
+        :param dummy: Unused input
+        :param p: Position of scrollbar from 0 to 1-ScrollerHeight
+        :return: None
+        """
 
-    fig, ax = plt.subplots(1, 1)
+        p_num = int(float(p) // ScrollerHeight)
+        scrollbar.set(float(p), float(p) + ScrollerHeight)
 
-    ax.plot(frequencies, measured, 'dimgray', label='measured', linewidth=1.0)
-    ax.plot(frequencies, reconstructed, 'r--', label='reconstructed', linewidth=1.0)
-    ax.set_yscale('log')
-    plt.legend()
-    plt.show()
+        reconstructed = np.abs(model.H[p_num, :])
+        frequencies = model.freq
+        measured = np.abs(model.frf[p_num, :])
+
+        ax.clear()
+        ax.plot(frequencies, measured, 'dimgray', label='measured', linewidth=1.0)
+        ax.plot(frequencies, reconstructed, 'r--', label='reconstructed', linewidth=1.0)
+        ax.set_yscale('log')
+        plt.title('FRF '+str(p_num+1))
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel('log magnitude ' + model.frf_type)
+        plt.legend()
+        canvas.draw()
+
+    # Work with model
+    num_of_locations = len(model.H[:, 0])
+    ScrollerHeight = 1 / num_of_locations
+
+    # Initialize tkinter
+    root = tk.Tk()
+    fig, ax = plt.subplots()
+
+    canvas = FigureCanvasTkAgg(fig, master=root)
+
+    # Tkinter app
+    frame = tk.Frame(root)
+    label = tk.Label(text='Use the scrollbar on the right to choose FRF')
+    label.pack()
+
+    scrollbar = tk.Scrollbar(root, command=plot)
+    scrollbar.pack(side='right', fill='y')
+    frame.pack()
+    scrollbar.set(0, ScrollerHeight)
+    plot(0, 0)
+    toolbar = NavigationToolbar2Tk(canvas, frame, pack_toolbar=False)
+    toolbar.update()
+    toolbar.pack(anchor='sw', side='bottom')
+    canvas.get_tk_widget().pack(expand=True, fill='both')
+
+    root.mainloop()
 
 
-reconstruct(acc, 0)
-reconstruct(acc, 1)
-reconstruct(acc, 2)
-reconstruct(acc, 3)
-reconstruct(acc, 4)
-reconstruct(acc, 5)
-reconstruct(acc, 6)
+reconstruct(acc)
 
 pass
