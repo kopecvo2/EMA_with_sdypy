83e2c644b51c113a9f8bab0be5214b6c5b58ac3e
kopecvo2 <81413409+kopecvo2@users.noreply.github.com>
2023-03-23 20:08:58 +0100

    approx nat freq not working for last part measurement, it has more stable poles

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ acc = EMA.Model(lower=10,
                 pol_order_high=100,
                 frf_from_uff=True)
 
-acc.read_uff('C:/Users/pc/Desktop/Scan_odlitek1_testP1_s1.UFF')
+acc.read_uff('C:/Users/pc/Desktop/Scan_odlitek5_testP1_s1.UFF')
 
 acc.get_poles(method='lscf', show_progress=True)
 
-approx_nat_freq = [1310, 1826, 3220, 3940, 5540, 5860, 6200, 6240, 7150, 7450, 7800, 8450, 8750, 8850, 9000, 9300, 9700]
+# acc.select_poles()
+
+approx_nat_freq = [1310, 1826, 3220, 3940, 5540, 5860, 6200, 6240, 7150, 7450, 7800, 8450, 8710, 8850, 9000, 9300, 9700]
 acc.select_closest_poles(approx_nat_freq)
 
 H, A = acc.get_constants(method='lsfd', f_lower=None)
@@ -28,6 +30,11 @@ H, A = acc.get_constants(method='lsfd', f_lower=None)
 MAC = acc.autoMAC()
 
 acc.print_modal_data()
+
+simplecheck = acc.nat_freq - approx_nat_freq
+
+print('Expected and found nat. freq. differences in Hz:')
+print(simplecheck)
 
 
 def reconstruct(model):
@@ -51,13 +58,20 @@ def reconstruct(model):
         frequencies = model.freq
         measured = np.abs(model.frf[p_num, :])
 
+        # fig.clear()
         ax.clear()
         ax.plot(frequencies, measured, 'dimgray', label='measured', linewidth=1.0)
         ax.plot(frequencies, reconstructed, 'r--', label='reconstructed', linewidth=1.0)
         ax.set_yscale('log')
         plt.title('FRF '+str(p_num+1))
         plt.xlabel('Frequency [Hz]')
-        plt.ylabel('log magnitude ' + model.frf_type)
+        ax.set_ylabel('log magnitude ' + model.frf_type)
+
+        ax2 = ax.twinx()
+        ax2.clear()
+        ax2.plot(approx_nat_freq, np.ones_like(approx_nat_freq), 'b+')
+        ax2.plot(model.nat_freq, np.ones_like(approx_nat_freq), 'r+')
+
         plt.legend()
         canvas.draw()
 
